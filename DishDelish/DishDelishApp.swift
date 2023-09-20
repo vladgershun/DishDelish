4466bcc80553f28a8123ca8b2e8c64a3bc583988
vladgershun <dev.vladgershun@gmail.com>
2023-09-20 16:15:32 -0700

    Initial Commit

---
+++ DishDelish/DishDelishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishDelishApp.swift
+//  DishDelish
+//
+//  Created by Vlad Gershun on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DishDelishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
